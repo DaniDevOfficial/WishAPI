9db497b7312c072c345b4c6b3f18cfab9a40e699
David <bischof.david.db@gmail.com>
2024-01-14 21:23:33 +0100

    added cors and many other security thingies

--- appSecure.js
+++ appSecure.js
@@ -2,17 +2,46 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const port = 3000;
 const secretKey = 'MeinGeheimesSchluesselwort'; // sollte aus einer Umgebungsvariable kommen
-const saltRounds = 10; 
+const saltRounds = 10;
+const cors = require('cors');
 
 // Benutzerdaten (normalerweise in einer Datenbank gespeichert)
 const users = [
     { id: 1, username: 'admin', hashedPassword: '', role: 'admin' },
     { id: 2, username: 'user', hashedPassword: '', role: 'user' },
 ];
+
+const allowedOrigins = ['https://david-bischof.ch'];
+// https://portfolio-dbischof.web.app/apitest hier funktioniert es nicht, da es nicht in der Liste der erlaubten Domänen ist
+// https://david-bischof.ch/apitest hier funktioniert es, da es in der Liste der erlaubten Domänen ist
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Unerlaubter Zugriff von der angegebenen Domäne.'));
+        }
+    },
+    optionsSuccessStatus: 200,
+
+};
+
+app.use(cors(corsOptions));
+
+
+
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 Minuten
+    max: 100, // Maximal 100 Anfragen für die gesamte API innerhalb des Zeitraums
+    message: 'Zu viele Anfragen von dieser IP, bitte versuchen Sie es später erneut.',
+});
+
+app.use(apiLimiter);
 
 // Middleware für die Authentifizierung
 const authenticateToken = (req, res, next) => {
@@ -26,7 +55,7 @@ const authenticateToken = (req, res, next) => {
     });
 };
 
-// Hashen des Passworts hier für jeden user das selbe Passwort
+// Hashen des Passworts hier für jeden user das selbe Passwort (real in der Datenbank gespeichert)
 users.forEach(user => {
     bcrypt.hash('admin123', saltRounds, (err, hash) => {
         if (!err) user.hashedPassword = hash;
@@ -63,6 +92,9 @@ app.post('/login', bodyParser.json(), (req, res) => {
     });
 });
 
+app.get('/example', (req, res) => {
+    res.send('Diese Route erlaubt nur Anfragen von https://david-bischof.ch.');
+});
 app.listen(port, () => {
     console.log(`Server läuft auf http://localhost:${port}`);
 });
